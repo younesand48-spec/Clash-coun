9d0b178d58d7db556f2cdfc2d694e4b8193f8073
younesand48-spec <238926551+younesand48-spec@users.noreply.github.com>
2025-12-18 06:12:49 +0100

    Initialize HTTP server in Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Clash counter OK");
+}).listen(PORT);
